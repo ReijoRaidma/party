28a0441d9fc495561196f3a25beac29e3e88373d
ReijoRaidma <Reijo@namespace.ee>
2016-09-14 10:08:00 +0300

    api

---
+++ parties/serializers.py
@@ -0,0 +1,35 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+from parties.models import Party, Guest
+
+
+class PartySerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='api:party-detail',
+    )
+    guests = serializers.HyperlinkedRelatedField(
+        view_name='api:guest-detail',
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Party
+        fields = (
+            'id',
+            'url',
+            'name',
+            'guests',
+        )
+
+
+class GuestSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='api:guest-detail'
+    )
+    class Meta:
+        model = Guest
+        fields = ('id','url','name', 'birth_date', 'party')
+
+
+
--- parties/urls.py
+++ parties/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url, include
+
+from rest_framework.routers import DefaultRouter
+
 from parties import views
+
+router = DefaultRouter()
+router.register(r'parties', views.PartyViewSet, base_name='party')
+router.register(r'guests', views.GuestViewSet, base_name='guest')
 
 
 urlpatterns = [
+    url(r'^api/', include(router.urls, namespace='api')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.party_list, name='party_list'),
     url(r'add_party', views.add_party, name='add_party'),
