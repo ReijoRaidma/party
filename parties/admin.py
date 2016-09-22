83994904f4a8290e6a7b22a49b177f4d8a1e87a0
ReijoRaidma <Reijo@namespace.ee>
2016-09-22 10:27:13 +0300

    Add django user admin

--- parties/admin.py
+++ parties/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from parties.models import Party, Guest
+from parties.models import Party, Guest, User
 
 admin.site.register(Party)
 admin.site.register(Guest)
+
+admin.site.register(User, UserAdmin)
