72a103e1139cb9842da8d0d4e4a706c8137f33fb
ReijoRaidma <Reijo@namespace.ee>
2016-09-23 12:44:11 +0300

    Urls working with uuid

--- parties/urls.py
+++ parties/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
                                namespace='rest_framework')),
     url(r'^$', views.party_list, name='party_list'),
     url(r'add_party', views.add_party, name='add_party'),
-    url(r'(?P<pk>\d+)/edit_party', views.edit_party, name='edit_party'),
-    url(r'(?P<pk>\d+)/', views.party_detail, name='detail'),
+    url(r'(?P<pk>[0-9a-z-]+)/edit_party', views.edit_party, name='edit_party'),
+    url(r'(?P<pk>[0-9a-z-]+)/', views.party_detail, name='detail'),
 
 
 ]
